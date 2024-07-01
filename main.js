2d67657f6156501f3a9bc4e431fd2a2137747951
Gabriel Martinello <martinellogabriel@gmail.com>
2024-07-01 12:23:47 -0300

    ajustes

--- main.js
+++ main.js
@@ -1,6 +1,69 @@
-onload = () => {
-    const c = setTimeout(() => {
-      document.getElementById('flowersContent').classList.remove("not-loaded");
-      clearTimeout(c);
-    }, 1000);
-  };
+function mostrarFlores() {
+  document.getElementById('title1').style.display = 'none';
+  document.getElementById('title2').style = 'block';
+  const c = setTimeout(() => {
+    document.getElementById('flowersContent').classList.remove("not-loaded");
+    clearTimeout(c);
+  }, 1000);
+}
+
+function redirectScreen() {
+  var input1 = document.getElementById('nomeInput').value;
+  var input2 = document.getElementById('dataNascInput').value;
+  var input3 = document.getElementById('anosInput').value;
+
+  if(input1 !== null && input2 !== null && input1 !== null) {
+      // Construir a query string
+      var queryString = `?input1=${encodeURIComponent(input1)}&input2=${encodeURIComponent(input2)}&input3=${encodeURIComponent(input3)}`;
+      window.location.href = 'flowers.html' + queryString;
+  }
+}
+
+function getParametros() {
+    var queryString = window.location.search;
+    queryString = queryString.substring(1);
+
+    var parametros = queryString.split('&');
+
+    var paramsObj = {};
+    parametros.forEach(param => {
+        var [key, value] = param.split('=');
+        paramsObj[key] = decodeURIComponent(value);
+    });
+
+    return paramsObj;
+}
+
+function getNomeCompleto() {
+  param = getParametros();
+  param = param.input1.replaceAll('%20', ' ');
+  return param;
+}
+
+function getDataNasc() {
+  param = getParametros();
+  return param.input2;
+}
+
+function getIdadeCalculada() {
+  param = getParametros();
+  dataNascimento = getDataNasc();
+
+  var partes = dataNascimento.split('/');
+  var dia = parseInt(partes[0], 10);
+  var mes = parseInt(partes[1], 10) - 1;
+  var ano = parseInt(partes[2], 10);
+
+  var dataNasc = new Date(ano, mes, dia);
+  var hoje = new Date();
+
+  var idade = hoje.getFullYear() - dataNasc.getFullYear();
+  var mesDiferenca = hoje.getMonth() - dataNasc.getMonth();
+
+  // Ajustar a idade se o aniversário ainda não tiver ocorrido no ano atual
+  if (mesDiferenca < 0 || (mesDiferenca === 0 && hoje.getDate() < dataNasc.getDate())) {
+      idade--;
+  }
+
+  return param.input3 - idade;
+}
